5a2126313975c15fbdf3ca4cbe953d0e8b063b11
chairyliu <jiyi_liu2001@163.com>
2024-01-14 02:12:36 +0800

    upadate code logic

--- src/main/java/cs/model/analysis/CrossFileRevisionAnalysis.java
+++ src/main/java/cs/model/analysis/CrossFileRevisionAnalysis.java
@@ -37,28 +37,30 @@ public class CrossFileRevisionAnalysis {
             return;
         }
 
-        // count
+        //所有srcpath下的方法总数，也就是分母
         this.methodDeclarationNum = 0;
+        //存在跨文件映射的方法，也就是分子
         this.crossTransferMethodDeclarationNum = 0;
 
-        //methodsList
+        //srcPathAndAllMethodsMap中第一个String代表srcpath，第二个List存放每一个srcpath下的所有改动过的方法声明，把所有方法汇总起来存到map里，dst同理
         this.srcPathAndMethodsMap = new HashMap<>();
         this.dstPathAndMethodsMap = new HashMap<>();
         List<String> srcMethodsList;
         List<String> dstMethodsList;
 
+        //因为在跨文件映射的时候，需要srcpath下的方法和其非对应dstpath下的所有方法比较，如果直接用dstPathAndMethodsMap，
+        //除了遍历路径外，还需要遍历每个list，会进行多层循环。所以用allDstMethods存储所有dstpath下的方法，减少了循环次数
         allDstMethods = new ArrayList<>();
 
+        //第一个String表示srcpath，第二个map集合绑定了每个方法和其相似度值，方便比较同一个方法同文件映射和跨文件映射的相似度大小
         Map<String, Map<String,Double>> mpSimMap = new HashMap<>();
+        //第一个参数是dst方法，第二个参数是其对应的dstpath。因为后续输出时，需要拿到每一个dst方法对应的路径
         Map<String, String> reverseProjectMap = new HashMap<>();
 
+        //这一块总体是遍历每个srcpath和dstpath，拿到其AST树和每个路径下的所有方法。再把ITree节点转换成字符串，存入对应的map集合中
         for (Map.Entry<String, String> entry : srcFilePathMap.entrySet()) {
             String srcPath = entry.getKey();
             String dstPath = entry.getValue();
-
-            // fresh the method list for each srcPath
-            srcMethodsList = new ArrayList<>();
-            dstMethodsList = new ArrayList<>();
 
             // get Content for both src and dst
             ByteArrayOutputStream srcFileStream = GitUtils
@@ -85,6 +87,9 @@ public class CrossFileRevisionAnalysis {
             } catch (Exception e) {
                 break;
             }
+            // fresh the method list for each srcPath
+            srcMethodsList = new ArrayList<>();
+            dstMethodsList = new ArrayList<>();
             // get all method declarations
             Set<ITree> srcMethodDeclarations = DeclarationUtil.getMethodDeclarations(tmp_src);
             for (ITree srcMethodDeclaration : srcMethodDeclarations) {
@@ -95,12 +100,7 @@ public class CrossFileRevisionAnalysis {
             for (ITree dstMethodDeclaration : dstMethodDeclarations) {
                 String dstMethod = TreePrinter(dstMethodDeclaration);
                 dstMethodsList.add(dstMethod);
-
             }
-
-            srcPathAndMethodsMap.put(srcPath, srcMethodsList);
-            dstPathAndMethodsMap.put(dstPath, dstMethodsList);
-
 
             // format printer step2:
             System.out.println("=================");
@@ -112,32 +112,42 @@ public class CrossFileRevisionAnalysis {
                 System.out.println(TreePrinter(srcMethodDeclaration));
             }
 
-            // update counter num;
+            // 每拿完一个srcpath下的所有方法，都会更新methodDeclarationNum的值
             this.methodDeclarationNum += srcMethodDeclarations.size();
 
-
-            // filtering unchanged methods
+            //过滤掉没有修改的方法。commonMethods表示src和dst里相同的方法，如果两个方法完全相同，那么就说明没有更改过，不考虑未修改的方法
             List<String> commonMethods = new ArrayList<>(srcMethodsList);
             commonMethods.retainAll(dstMethodsList);
+            //filterSrcMethodList表示已经过滤后的方法集合，只包含修改过的语句
             List<String> filterSrcMethodList = new ArrayList<>(srcMethodsList);
             List<String> filterDstMethodList = new ArrayList<>(dstMethodsList);
             filterSrcMethodList.removeAll(commonMethods);
             filterDstMethodList.removeAll(commonMethods);
+//            System.out.println("6");
+
+            srcPathAndMethodsMap.put(srcPath, filterSrcMethodList);
+            dstPathAndMethodsMap.put(dstPath, filterDstMethodList);
+
+            //对于每个dst中的方法，都将它和其dstpath绑定起来，方便后面输出
             for (String filterDstMethod : filterDstMethodList) {
                 reverseProjectMap.put(filterDstMethod, dstPath);
             }
+            //每一次方法过滤后，都同步更新allDstMethods列表，所有dstpath都遍历一遍后，该参数内存着所有修改过的dst方法
             allDstMethods.addAll(filterDstMethodList);
 
+            //mpSimMap的第二个参数，将每个方法和其相似度值绑定
             HashMap<String, Double> methodToSimMap = new HashMap<>();
 
             for (String sameFile_srcMethodDeclaration : filterSrcMethodList) {
                 for (String sameFile_dstMethodDeclaration : filterDstMethodList) {
+                    //src和dst路径相同时，获取每个方法之间的相似度
                     double sameFile_similarity = compareTwo(sameFile_srcMethodDeclaration, sameFile_dstMethodDeclaration);
+                    //将src的方法和相似度绑定，方便后面和跨文件的相似度进行对比
                     methodToSimMap.put(sameFile_srcMethodDeclaration, sameFile_similarity);
+                    //定义max_similarity用来更迭src方法最大的相似度
                     Double max_similarity = methodToSimMap.getOrDefault(sameFile_srcMethodDeclaration, 0.0);
                     if (max_similarity <= sameFile_similarity) {
                         methodToSimMap.put(sameFile_srcMethodDeclaration, sameFile_similarity);
-                        methodToSimMap.put(sameFile_dstMethodDeclaration, sameFile_similarity);
 //                        System.out.println("123");
                     }
                 }
@@ -148,38 +158,33 @@ public class CrossFileRevisionAnalysis {
         for (Map.Entry<String, List<String>> entry : srcPathAndMethodsMap.entrySet()) {
             String srcPath = entry.getKey();
             List<String> srcMethodList = entry.getValue();
+            //获取当前dstpath下所有修改过的方法
             List<String> curDstMethods = dstPathAndMethodsMap.get(srcPath);
+            //初始化一个crossDstMethods，现在里面存了所有dstpath下的所有修改方法
             List<String> crossDstMethods = new ArrayList<>(allDstMethods);
+            //从crossDstMethods中移除掉当前dstpath下的修改方法，剩下的方法列表就是用于跨文件映射的方法
             crossDstMethods.removeAll(curDstMethods);
             for (String srcMethod : srcMethodList) {
                 int flag = 0;
                 for (String dstMethod : crossDstMethods) {
                     double cross_similarity = compareTwo(srcMethod, dstMethod);
                     String dstPath = reverseProjectMap.get(dstMethod);
-                    double srcSameFileSimilarity = 0.0;
-                    double dstSameFileSimilarity = 0.0;
+                    double sameFile_similarity = 0.0;
                     if (cross_similarity >= 0.8) {
-                        Map<String, Double> srcMethodToSimMap = mpSimMap.getOrDefault(srcPath, null);
-                        Map<String, Double> dstMethodToSimMap = mpSimMap.getOrDefault(dstPath, null);
-                        if (srcMethodToSimMap == null) {
-                            srcSameFileSimilarity = 0.0;
+                        Map<String, Double> methodToSimMap = mpSimMap.getOrDefault(srcPath, null);
+                        if (methodToSimMap == null) {
+                            sameFile_similarity = 0.0;
                         }
                         else {
-                            srcSameFileSimilarity = srcMethodToSimMap.getOrDefault(srcMethod, 0.0);
+                            sameFile_similarity = methodToSimMap.getOrDefault(srcMethod, 0.0);
                         }
-
-                        if (dstMethodToSimMap == null) {
-                            dstSameFileSimilarity = 0.0;
-                        }
-                        else{
-                            dstSameFileSimilarity = dstMethodToSimMap.getOrDefault(dstMethod, 0.0);
-                        }
-                        if (srcSameFileSimilarity < 1.0) {
-                            if (srcSameFileSimilarity < cross_similarity && dstSameFileSimilarity < cross_similarity) {
+                        if (sameFile_similarity < 1.0) {
+                            if (sameFile_similarity < cross_similarity) {
                                 String r = "In file: " + srcPath + "\n" + "[" + srcMethod + "] ----> "
                                                 + "\n" + reverseProjectMap.get(dstMethod) + "-[" + dstMethod + "]"
                                                 + "\n" + "Similarity: " + cross_similarity + "\n\n";
                                 System.out.print(r);
+                                //存储了所有跨文件映射的方法，用于后续集体输出
                                 crossList.add(r);
                                 flag = 1;
 //                                  break;

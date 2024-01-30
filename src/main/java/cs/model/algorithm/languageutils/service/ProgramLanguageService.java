14ed4bd2928b5a6b9f0a01c1f6d80becb244015d
chairyliu <jiyi_liu2001@163.com>
2024-01-30 19:13:33 +0800

    delete three useless code files

--- src/main/java/cs/model/algorithm/languageutils/service/ProgramLanguageService.java
+++
@@ -1,6 +0,0 @@
-package cs.model.algorithm.languageutils.service;
-
-public interface ProgramLanguageService {
-
-    String removeCommentAndBlank(String codeStr);
-}
--- src/test/java/ase2023/Comparison.java
+++ src/test/java/ase2023/Comparison.java
@@ -201,7 +201,7 @@ public class Comparison {
     public static void main(String[] args) {
         // project name
         String projectPathA = "C:\\Users\\29366\\Desktop\\iASTMapper\\ase2023\\iASTMapper_res20240130180131\\activemq";
-        String projectPathB = "C:\\Users\\29366\\Desktop\\iASTMapper\\ase2023\\iASTMapper_res20240130165631\\activemq";
+        String projectPathB = "C:\\Users\\29366\\Desktop\\iASTMapper\\ase2023\\iASTMapper_res20240130191128\\activemq";
 //        System.out.println(projectPath);
         // we only need to compare the first 100 commits,
         // and projectA and projectB should have the same commits lists
